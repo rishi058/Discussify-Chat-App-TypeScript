14e6429dbdf1580a72f26bc17fe5939fda3ed41d
Rishi <rishi.helloworld@gmail.com>
2024-04-20 12:38:21 +0530

    Setup typescript backend

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+// Importing module
+import express from "express";
+
+const app = express();
+const PORT: Number = 3000;
+
+// Handling GET / Request
+app.get("/", (req, res) => {
+  res.send("Welcome to typescript backend!");
+});
+
+// Server setup
+app.listen(PORT, () => {
+  console.log(
+    "The application is listening " + "on port http://localhost:" + PORT
+  );
+});
